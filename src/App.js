f5fcb80787b0f91b555f5f5782e843a50d02f17e
Madihakhan16 <2023madiha.khan@vidyashilp.edu.in>
2026-06-13 01:37:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<ConcordHome />} />
         <Route path="/about" element={<AboutUs />} />
-        <Route path="/technologies" element={<ComingSoon page="Technologies" />} />
-        <Route path="/solutions" element={<ComingSoon page="Products & Solutions" />} />
-        <Route path="/projects" element={<ComingSoon page="Projects" />} />
-        <Route path="/investors" element={<ComingSoon page="Investors" />} />
-        <Route path="/media" element={<ComingSoon page="Media" />} />
-        <Route path="/careers" element={<ComingSoon page="Careers" />} />
-        <Route path="/contact" element={<ComingSoon page="Contact Us" />} />
-        <Route path="*" element={<ComingSoon page="Page Not Found" />} />
+        <Route path="*" element={<ComingSoon page="This Page" />} />
       </Routes>
     </BrowserRouter>
   );
