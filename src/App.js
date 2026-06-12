f2672109aac1c3d687c76da2a52196d66255ef69
Madihakhan16 <2023madiha.khan@vidyashilp.edu.in>
2026-06-13 01:12:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ConcordHome from './ConcordHome';
 import AboutUs from './AboutUs';
+import ComingSoon from './ComingSoon';
 
 export default function App() {
   return (
@@ -8,6 +9,14 @@ export default function App() {
       <Routes>
         <Route path="/" element={<ConcordHome />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/technologies" element={<ComingSoon page="Technologies" />} />
+        <Route path="/solutions" element={<ComingSoon page="Products & Solutions" />} />
+        <Route path="/projects" element={<ComingSoon page="Projects" />} />
+        <Route path="/investors" element={<ComingSoon page="Investors" />} />
+        <Route path="/media" element={<ComingSoon page="Media" />} />
+        <Route path="/careers" element={<ComingSoon page="Careers" />} />
+        <Route path="/contact" element={<ComingSoon page="Contact Us" />} />
+        <Route path="*" element={<ComingSoon page="Page Not Found" />} />
       </Routes>
     </BrowserRouter>
   );
